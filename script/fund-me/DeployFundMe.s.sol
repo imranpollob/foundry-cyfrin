37446bb12ea75ae7214b12effebab27d98921f58
Imran Pollob <polboy777@gmail.com>
2024-04-30 18:27:35 -0400

    import deployscript into test file

--- script/fund-me/DeployFundMe.s.sol
+++ script/fund-me/DeployFundMe.s.sol
@@ -6,9 +6,10 @@ import {FundMe} from "./../../src/fund-me/FundMe.sol";
 
 contract DeployFundMe is Script {
     // forge script script/fund-me/DeployFundMe.s.sol
-    function run() external {
+    function run() external returns (FundMe) {
         vm.startBroadcast();
-        new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         vm.stopBroadcast();
+        return fundMe;
     }
 }
--- test/fund-me/FundMeTest.t.sol
+++ test/fund-me/FundMeTest.t.sol
@@ -3,22 +3,35 @@ pragma solidity ^0.8.20;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "./../../src/fund-me/FundMe.sol";
+import {DeployFundMe} from "./../../script/fund-me/DeployFundMe.s.sol";
 
+// forge test --match-contract FundMeTest -vvv
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumUsdIsFive() public view {
         assert(fundMe.MINIMUM_USD() == 5e18);
     }
 
-    function testOwner() public {
+    function testOwner() public view {
         console.log(fundMe.i_owner());
         console.log(msg.sender);
         // for FundMe, FundMeTest is the msg.sender not us
         assert(fundMe.i_owner() == address(this));
+    }
+
+    // forge test --match-test testPriceFeedVersion -vvvv
+    // It will throw error
+    // as we don't have access to sepolia's deployed AggregatorV3Interface address
+    // 0x694AA1769357215DE4FAC081bf1f309aDC325306::version() [staticcall]
+    // forge test --match-test testPriceFeedVersion -vvv --rpc-url https://1rpc.io/sepolia
+    function testPriceFeedVersion() public view {
+        assert(fundMe.getVersion() == 4);
     }
 }
